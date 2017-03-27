8ee2d114a20c927db730868244ff5b458c9925e6
sergtihonov77 <sergtihonov77@gmail.com>
2017-03-27 03:59:32 +0300

    Changing task conditions vol.1

--- iText/Controllers/HomeController.cs
+++ iText/Controllers/HomeController.cs
@@ -22,19 +22,23 @@ namespace iText.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ColumnWidth_1, ColumnWidth_2, ColumnWidth_3, ColumnText_1, ColumnText_2, ColumnText_3")]Columns model)
+        public ActionResult Create([Bind(Include = "ColumnWidth_1, ColumnWidth_3, ColumnText_1, ColumnText_2, ColumnText_3")]Columns model)
         {
             if (ModelState.IsValid)
             {
-                Document pdf = new Document(iTextSharp.text.PageSize.A4);
+                Rectangle main = new Rectangle(1, 1, 1000, 1000);
+                Document pdf = new Document(main);
                 FileStream st = new FileStream(Server.MapPath("~/Data/data.pdf"), FileMode.Create);
                 PdfWriter wr = PdfWriter.GetInstance(pdf, st);
+                
 
                 pdf.Open();
 
                 BaseFont baseFont = BaseFont.CreateFont(Server.MapPath("~/fonts/ARIAL.TTF"), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
                 iTextSharp.text.Font font = new iTextSharp.text.Font(baseFont, iTextSharp.text.Font.DEFAULTSIZE, iTextSharp.text.Font.NORMAL);
 
+                //Old
+                /*
                 PdfPTable table = new PdfPTable(3);
 
                 PdfPCell cell1 = new PdfPCell(new Phrase(model.ColumnText_1.ToString(),font));
@@ -56,6 +60,41 @@ namespace iText.Controllers
                 float[] widths = new float[] { model.ColumnWidth_1, model.ColumnWidth_2, model.ColumnWidth_3};
                 table.SetWidths(widths);
                 pdf.Add(table);
+                */
+
+                PdfContentByte cb = wr.DirectContent;
+
+                var rec1 = new iTextSharp.text.Rectangle(10, 950, model.ColumnWidth_1, 850);
+                rec1.Border = iTextSharp.text.Rectangle.BOX;
+                rec1.BorderWidth = 1;
+                rec1.BorderColor = new BaseColor(0, 0, 0);
+                cb.Rectangle(rec1);              
+                ColumnText column1 = new ColumnText(cb);
+                column1.SetSimpleColumn(rec1);            
+                column1.AddText(new Phrase(model.ColumnText_1.ToString(), font));
+                column1.Go();
+
+                var rec2 = new iTextSharp.text.Rectangle(10 + model.ColumnWidth_1, 950, 1000 - 20 - model.ColumnWidth_3, 850);
+                rec2.Border = iTextSharp.text.Rectangle.BOX;
+                rec2.BorderWidth = 1;
+                rec2.BorderColor = new BaseColor(0, 0, 0);
+                cb.Rectangle(rec2);
+                ColumnText column2 = new ColumnText(cb);
+                column2.SetSimpleColumn(rec2);
+                column2.AddText(new Phrase(model.ColumnText_2.ToString(), font));
+                column2.Go();
+
+                var rec3 = new iTextSharp.text.Rectangle(990 - model.ColumnWidth_3, 950, 990, 850);
+                rec3.Border = iTextSharp.text.Rectangle.BOX;
+                rec3.BorderWidth = 1;
+                rec3.BorderColor = new BaseColor(0, 0, 0);
+                cb.Rectangle(rec3);
+                ColumnText column3 = new ColumnText(cb);
+                column3.SetSimpleColumn(rec3);
+                column3.AddText(new Phrase(model.ColumnText_3.ToString(), font));
+                column3.Go();
+                
+
                 pdf.Close();
 
 
--- iText/Models/Columns.cs
+++ iText/Models/Columns.cs
@@ -9,15 +9,11 @@ namespace iText.Models
     public class Columns
     {
         [Required]
-        [Range(1,50, ErrorMessage = "The length of the column should be in the range from 1 to 50")]
+        [Range(10, 900, ErrorMessage = "The length of the column should be in the range from 10 to 900")]
         public int ColumnWidth_1 { get; set; }
 
         [Required]
-        [Range(1, 50, ErrorMessage = "The length of the column should be in the range from 1 to 50")]
-        public int ColumnWidth_2 { get; set; }
-
-        [Required]
-        [Range(1, 50, ErrorMessage = "The length of the column should be in the range from 1 to 50")]
+        [Range(10, 900, ErrorMessage = "The length of the column should be in the range from 10 to 900")]
         public int ColumnWidth_3 { get; set; }
 
         [Required]
